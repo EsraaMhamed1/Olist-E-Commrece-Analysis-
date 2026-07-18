2ddeadcbf3e89dc953f0858bc8ca6727d65ff746
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-07-18 18:04:32 +0200

    Update 06.EDA.sql

--- 03.SQL/06.EDA.sql
+++ 03.SQL/06.EDA.sql
@@ -1,1 +1,31 @@
+-- KPIS
+--------
+go
+
+-- Total orders 
+SELECT Count(*) AS total_orders
+FROM   orders;
+
+go
+
+--  successful orders  received 
+SELECT Count(*) AS successful_orders
+FROM   orders
+WHERE  order_status = 'delivered';
+
+go
+
+-- successful order rate
+SELECT Count(*)                             AS total_orders,
+       Sum(CASE
+             WHEN order_status = 'delivered' THEN 1
+             ELSE 0
+           END)                             AS successful_orders,
+       Round(Sum(CASE
+                   WHEN order_status = 'delivered' THEN 1
+                   ELSE 0
+                 END) * 100.0 / Count(*), 2)AS successful_rate
+FROM   orders;
+
+go 
 
