b05956dd4696d2857b484387d66b107be9f5673e
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-07-22 22:44:22 +0200

    Customers by State

--- 03.SQL/06.EDA/04_Customer_Analysis.sql
+++ 03.SQL/06.EDA/04_Customer_Analysis.sql
@@ -1,1 +1,12 @@
+--1. Customers by State
+go 
+
+select * from customers ; 
+go 
+
+select customer_state , count(distinct customer_unique_id) as total_customers
+from customers 
+group by customer_state 
+order by total_customers desc; 
+go 
 
