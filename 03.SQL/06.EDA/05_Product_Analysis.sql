820000801b17891a32841578a3e0de65e3560be8
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-07-26 22:05:02 +0200

    Update 05_Product_Analysis.sql

--- 03.SQL/06.EDA/05_Product_Analysis.sql
+++ 03.SQL/06.EDA/05_Product_Analysis.sql
@@ -20,6 +20,15 @@ go
 
 
 --2. Highest & Lowest Priced Products
+-------------------------------------
+
 --3. Products with No Sales
+---------------------------
+
+
 --4. Product Category Distribution
+----------------------------------
+
+
 --5. Product Size & Weight Analysis
+-----------------------------------
