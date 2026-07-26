5f12cf072e289f716ecfcb53a0cf1c922eb59d50
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-07-26 21:30:52 +0200

    Update 05_Product_Analysis.sql

--- 03.SQL/06.EDA/05_Product_Analysis.sql
+++ 03.SQL/06.EDA/05_Product_Analysis.sql
@@ -1,5 +1,24 @@
 
 --1. Top Selling Products
+-------------------------
+go 
+
+select * from orders ; 
+go 
+
+select * from order_items ;
+go 
+
+select top(10) oi.product_id ,count(oi.product_id) as products_sold 
+from orders o 
+join order_items oi 
+on o.order_id = oi.order_id
+where order_status = 'delivered' 
+group by oi.product_id 
+order by products_sold desc ; 
+go 
+
+
 --2. Highest & Lowest Priced Products
 --3. Products with No Sales
 --4. Product Category Distribution
