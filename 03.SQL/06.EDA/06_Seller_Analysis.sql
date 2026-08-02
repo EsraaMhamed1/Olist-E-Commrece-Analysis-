ef13de0cd015119ed9ef9ce7069ed4f8d0918980
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-02 21:56:14 +0200

    Add revenue analysis by seller state
    Added queries to analyze revenue by seller state and retrieve seller, order, and order item data.

--- 03.SQL/06.EDA/06_Seller_Analysis.sql
+++ 03.SQL/06.EDA/06_Seller_Analysis.sql
@@ -37,6 +37,32 @@ group by seller_state
 order by num_of_sellers desc; 
 go 
 
+  
 -- 3. Revenue by Seller State
+-----------------------------
+go 
+
+select * from sellers ; 
+go 
+
+select * from orders ; 
+go 
+
+select * from order_items ; 
+go 
+
+select s.seller_state , round(sum(oi.price) ,2)as Revenue 
+from sellers s 
+join order_items oi 
+on s.seller_id = oi.seller_id
+join orders o 
+on o.order_id = oi.order_id 
+where order_status = 'delivered'
+group by s.seller_state 
+order by Revenue desc ; 
+go 
+
+
 -- 4. Average Revenue per Seller
+--------------------------------
 -- 5. Sellers with the Most Products Sold
