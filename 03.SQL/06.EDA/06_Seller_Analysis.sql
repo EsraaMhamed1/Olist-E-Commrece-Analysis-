6bc19efb378379271a959aa0a1cfecf1bd1e2936
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-08-02 20:10:19 +0200

    Add SQL queries for seller analysis

--- 03.SQL/06.EDA/06_Seller_Analysis.sql
+++ 03.SQL/06.EDA/06_Seller_Analysis.sql
@@ -1,5 +1,30 @@
 
 -- 1. Top 10 Sellers by Revenue
+-------------------------------
+go 
+
+select * from sellers ;
+go 
+
+select * from order_items ; 
+go 
+
+select * from orders ; 
+go 
+
+select top(10) s.seller_state, s.seller_id , round(sum(oi.price) ,2)as Revenue 
+from sellers s 
+join order_items oi 
+on s.seller_id = oi.seller_id 
+join orders o 
+on o.order_id = oi.order_id 
+where order_status = 'delivered' 
+group by s.seller_id, s.seller_state 
+order by Revenue desc; 
+go 
+
+
+
 -- 2. Sellers Distribution by State
 -- 3. Revenue by Seller State
 -- 4. Average Revenue per Seller
